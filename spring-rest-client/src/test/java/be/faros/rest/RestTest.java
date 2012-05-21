8a3894e9ccc8dd1a5859dc8e4910dbf0bb885872
Jeroen Sterken <jeroen.sterken@gmail.com>
2012-05-21 15:43:29 +0200

    imports updated

--- spring-rest-client/src/test/java/be/faros/rest/RestTest.java
+++ spring-rest-client/src/test/java/be/faros/rest/RestTest.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.CommonsClientHttpRequestFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
